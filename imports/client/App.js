d648794353d45fd38e8ffc5f4d39c54296744168
yoblig <gilboyk@gmail.com>
2017-01-15 23:04:47 -0600

    collection is setup

---
+++ imports/client/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+    headingClick(){
+        this.setState({count: this.state.count + 1});
+    }
+
+  render(){
+    return (
+      <header onClick={this.headingClick.bind(this)}>
+        <Heading count={this.state.count} />
+      </header>
+    );
+  }
+}
+
+class Heading extends Component {
+    render(){
+        return (
+            <h1>{this.props.count}</h1>
+        );
+    }
+}
+
+export default App;
--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
+const Tests = new Mongo.Collection('tests');
+
 Meteor.startup(() => {
   // code to run on server at startup
+  Tests.insert({
+    name: 'Money',
+    value: 1
+  });
 });
