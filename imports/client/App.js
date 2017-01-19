2df8f767b950a3b8c83d927ce8160f6152ec87cd
yoblig <gilboyk@gmail.com>
2017-01-18 22:37:11 -0600

    form is inserting into collection

--- imports/client/App.js
+++ imports/client/App.js
@@ -4,6 +4,30 @@ import Items from '../api/Items';
 import Item from './Item';
 
 class App extends Component {
+
+    addItems(event){
+        event.preventDefault();
+        const itemOne = this.refs.itemOne.value;
+        const itemTwo = this.refs.itemTwo.value;
+
+        if(itemOne != '' && itemTwo != ''){
+            Items.insert({
+            itemOne: {
+                text: itemOne,
+                value: 0
+            },
+            itemTwo: {
+                text: itemTwo,
+                value: 0
+            }
+        });
+            this.refs.itemOne.value = '';
+            this.refs.itemTwo.value = '';
+        }
+        
+        
+    }
+
   render(){
     return (
         <div>
@@ -11,9 +35,16 @@ class App extends Component {
                 LevelUp Voting
             </header>
             <main>
+
+            <form onSubmit={this.addItems.bind(this)}>
+                <input type='text' ref='itemOne' />
+                <input type='text' ref='itemTwo' />
+                <button type='submit'>Add Item</button>
+            </form>
+
             {this.props.items.map((item) => {
                 return(
-                    <Item item={item} />
+                    <Item item={item} key={item._id} />
                 )
             })}
             </main>
--- server/main.js
+++ server/main.js
@@ -3,14 +3,5 @@ import Items from '../imports/api/Items';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  Items.insert({
-    itemOne: {
-      text: 'money',
-      value: 0
-    },
-    itemTwo: {
-      text: 'cash',
-      value: 0
-    }
-  });
+  
 });
